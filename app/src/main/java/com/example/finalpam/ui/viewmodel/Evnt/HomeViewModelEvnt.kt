e2a60f43800f2d85e203ce42519227a0e8468a6e
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-28 04:22:29 +0700

    Membuat class HomeViewModelEvnt dengan val evt

---
+++ app/src/main/java/com/example/finalpam/ui/viewmodel/Evnt/HomeViewModelEvnt.kt
@@ -0,0 +1,13 @@
+package com.example.finalpam.ui.viewmodel.Evnt
+
+
+import androidx.lifecycle.ViewModel
+
+import com.example.finalpam.repository.EventRepository
+
+
+
+
+class HomeViewModelEvnt(
+    private val evt: EventRepository
+): ViewModel()
