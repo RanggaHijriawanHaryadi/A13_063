1b90b8ca2b2ab3e749b9149f1935d35c5cedf2cf
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-27 22:40:16 +0700

    Membuat Update peserta dengan navigasi DestinasiUpdatePstr

--- app/src/main/java/com/example/finalpam/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/finalpam/ui/navigation/PengelolaHalaman.kt
@@ -8,23 +8,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.example.finalpam.ui.view.Event.DetailViewEvent
-import com.example.finalpam.ui.view.Event.HomeViewEvent
-import com.example.finalpam.ui.view.Event.InsertViewEvent
-import com.example.finalpam.ui.view.Event.UpdateViewEvent
-import com.example.finalpam.ui.view.HalamanUtamaView.HalamanUtamaView
-import com.example.finalpam.ui.view.Tikets.DetailViewTikets
 import com.example.finalpam.ui.view.peserta.DetailViewPstr
 import com.example.finalpam.ui.view.peserta.HomeViewPeserta
 import com.example.finalpam.ui.view.peserta.InsertViewPstr
-import com.example.finalpam.ui.view.peserta.UpdateViewPstr
-import com.example.finalpam.ui.view.Tikets.HomeViewTikets
-import com.example.finalpam.ui.view.Tikets.InsertViewTikets
-import com.example.finalpam.ui.view.Tikets.UpdateViewTikets
-import com.example.finalpam.ui.view.Transaksi.DetailViewTransaksi
-import com.example.finalpam.ui.view.Transaksi.HomeViewTransaksi
-import com.example.finalpam.ui.view.Transaksi.InsertViewTransaksi
-import com.example.finalpam.ui.view.Transaksi.UpdateViewTransaksi
 
 
 @Composable
