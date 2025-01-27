e91f2129b66bc8b3c3efe1ffe31bb59802d72c80
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-28 05:36:42 +0700

    Membuat DetailViewPstr dengan topAppBar dengan icon edit

--- app/src/main/java/com/example/finalpam/ui/view/Event/DetailViewEvent.kt
+++ app/src/main/java/com/example/finalpam/ui/view/Event/DetailViewEvent.kt
@@ -1,16 +1,21 @@
 package com.example.finalpam.ui.view.Event
 
 import androidx.compose.foundation.layout.Arrangement
-
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,12 +24,89 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.finalpam.entitas.Event
+import com.example.finalpam.ui.costumewidget.TopAppBar
+import com.example.finalpam.ui.navigation.DestinasiDetailEvnt
+import com.example.finalpam.ui.viewmodel.Evnt.DetailUiEvntState
+import com.example.finalpam.ui.viewmodel.Evnt.DetailViewModelEvnt
+import com.example.finalpam.ui.viewmodel.PenyediaViewModel
 
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailViewEvent(
+    modifier: Modifier = Modifier,
+    onBackClick: () -> Unit,
+    onEditClick: () -> Unit,
+    viewModel: DetailViewModelEvnt = viewModel(factory = PenyediaViewModel.Factory)
+) {
 
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = DestinasiDetailEvnt.titleRes,
+                canNavigateBack = true,
+                navigateUp = onBackClick,
+                onRefresh = {
+                    viewModel.getEventById()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onEditClick,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Event"
+                )
+            }
+        }
+    ) { innerPadding ->
 
+        BodyDetailEvnt(
+            modifier = Modifier.padding(innerPadding),
+            detailUiEvntState = viewModel.detailUiEvntState,
+            retryAction = { viewModel.getEventById() }
+        )
+
+    }
+}
+
+@Composable
+fun BodyDetailEvnt(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    detailUiEvntState: DetailUiEvntState
+) {
+    when (detailUiEvntState) {
+        is DetailUiEvntState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+
+        is DetailUiEvntState.Success -> {
+            if (detailUiEvntState.event.data.id_event == 0) {
+                Box(
+                    modifier = modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text("Data tidak ditemukan.")
+                }
+            } else {
+                ItemDetailEvnt(
+                    event = detailUiEvntState.event.data,
+                    modifier = modifier.padding(16.dp)
+                )
+            }
+        }
+
+        is DetailUiEvntState.Error -> OnError(
+            retryAction,
+            modifier = modifier.fillMaxSize()
+        )
+    }
+}
 
 @Composable
 fun ItemDetailEvnt(
