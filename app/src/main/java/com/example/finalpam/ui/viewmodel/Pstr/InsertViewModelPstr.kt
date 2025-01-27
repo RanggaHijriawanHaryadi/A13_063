c3712a531b75d05ec9bb8ebeda9301e50f14072a
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-27 20:49:44 +0700

    Membuat class InsertViewModelPstr dengan InsertUiPesertaState,InsertUiPesertaEvent

--- app/src/main/java/com/example/finalpam/ui/viewmodel/Pstr/InsertViewModelPstr.kt
+++ app/src/main/java/com/example/finalpam/ui/viewmodel/Pstr/InsertViewModelPstr.kt
@@ -1,10 +1,33 @@
 package com.example.finalpam.ui.viewmodel.Pstr
 
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.finalpam.entitas.Peserta
+import com.example.finalpam.repository.PesertaRepository
+import kotlinx.coroutines.launch
 
+class InsertViewModelPstr (
+    private val pstr: PesertaRepository
+): ViewModel() {
 
-
+    var uiStatePstr by mutableStateOf(InsertUiPesertaState())
+        private set
+    fun updateInsertPstrState(insertUiPesertaEvent: InsertUiPesertaEvent) {
+        uiStatePstr = InsertUiPesertaState(insertUiPesertaEvent = insertUiPesertaEvent)
+    }
+    suspend fun insertPstr(){
+        viewModelScope.launch {
+            try {
+                pstr.insertPeserta(uiStatePstr.insertUiPesertaEvent.toPstr())
+            } catch (e:Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 data class InsertUiPesertaState(
     val insertUiPesertaEvent: InsertUiPesertaEvent = InsertUiPesertaEvent(),
