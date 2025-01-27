f3158094d9b5a21deb539951946963b315172d7c
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-28 05:26:29 +0700

    Membuat EventCard dengan card data event dan DeleteConfirmationDialog

--- app/src/main/java/com/example/finalpam/ui/view/Event/HomeViewEvent.kt
+++ app/src/main/java/com/example/finalpam/ui/view/Event/HomeViewEvent.kt
@@ -1,21 +1,112 @@
 package com.example.finalpam.ui.view.Event
 
 
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Arrangement
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.finalpam.entitas.Event
 
 
 
+
+
+@Composable
+fun EventCard(
+    event: Event,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Event) -> Unit = {}
+) {
+    var showDialog by remember { mutableStateOf(false) }
+
+    if (showDialog) {
+        DeleteConfirmationDialog(
+            onDeleteConfirm = {
+                showDialog = false
+                onDeleteClick(event)
+            },
+            onDeleteCancel = {
+                showDialog = false
+            }
+        )
+    }
+
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Icon(
+                imageVector = Icons.Filled.DateRange,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(90.dp)
+                    .padding(18.dp)
+            )
+            Column (
+                modifier = Modifier.padding(14.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ){
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    DetailEvnt(judul = "Id_Event", isinya = event.id_event.toString())
+                    Spacer(Modifier.weight(1f))
+                    IconButton(onClick = { showDialog = true }) {
+                        Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = null,
+                        )
+                    }
+                }
+                Text(
+                    text = event.nama_event,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Text(
+                    text = "tanggal: ${event.tanggal_event}",
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Text(
+                    text = "lokasi: ${event.lokasi_event}",
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun DetailEvnt(
