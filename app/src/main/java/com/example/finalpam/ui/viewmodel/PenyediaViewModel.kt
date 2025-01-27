7d86de64bf79081db267db63a46ebb6847ff3e31
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-28 04:58:18 +0700

    Membuat initializer pada home,insert,detail,update

--- app/src/main/java/com/example/finalpam/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/finalpam/ui/viewmodel/PenyediaViewModel.kt
@@ -6,10 +6,22 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.finalpam.FestivalApplications
+import com.example.finalpam.ui.viewmodel.Evnt.DetailViewModelEvnt
+import com.example.finalpam.ui.viewmodel.Evnt.HomeViewModelEvnt
+import com.example.finalpam.ui.viewmodel.Evnt.InsertViewModelEvnt
+import com.example.finalpam.ui.viewmodel.Evnt.UpdateViewModelEvnt
 import com.example.finalpam.ui.viewmodel.Pstr.DetailViewModelPstr
 import com.example.finalpam.ui.viewmodel.Pstr.HomeViewModelPstr
 import com.example.finalpam.ui.viewmodel.Pstr.InsertViewModelPstr
 import com.example.finalpam.ui.viewmodel.Pstr.UpdateViewModelPstr
+import com.example.finalpam.ui.viewmodel.Tkts.DetailViewModelTkts
+import com.example.finalpam.ui.viewmodel.Tkts.HomeViewModelTkts
+import com.example.finalpam.ui.viewmodel.Tkts.InsertViewModelTkts
+import com.example.finalpam.ui.viewmodel.Tkts.UpdateViewModelTkts
+import com.example.finalpam.ui.viewmodel.Tski.DetailViewModelTski
+import com.example.finalpam.ui.viewmodel.Tski.HomeViewModelTski
+import com.example.finalpam.ui.viewmodel.Tski.InsertViewModelTski
+import com.example.finalpam.ui.viewmodel.Tski.UpdateViewModelTski
 
 
 object PenyediaViewModel {
@@ -27,6 +39,20 @@ object PenyediaViewModel {
         }
         initializer {
             UpdateViewModelPstr(createSavedStateHandle(),aplikasiFsvl().container.pesertaRepository)
+        }
+
+        // Untuk Event
+        initializer {
+            HomeViewModelEvnt(aplikasiFsvl().container.eventRepository)
+        }
+        initializer {
+            InsertViewModelEvnt(aplikasiFsvl().container.eventRepository)
+        }
+        initializer {
+            DetailViewModelEvnt(createSavedStateHandle(),aplikasiFsvl().container.eventRepository)
+        }
+        initializer {
+            UpdateViewModelEvnt(createSavedStateHandle(),aplikasiFsvl().container.eventRepository)
         }
     }
 }
