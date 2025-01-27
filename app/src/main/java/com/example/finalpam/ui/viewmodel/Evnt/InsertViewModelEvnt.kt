48736c9a9507ae4d45f7cbb87cf1a43a27427d0d
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-28 04:35:47 +0700

    Menambahkan InsertUiEvntEvent dengan data Event

--- app/src/main/java/com/example/finalpam/ui/viewmodel/Evnt/InsertViewModelEvnt.kt
+++ app/src/main/java/com/example/finalpam/ui/viewmodel/Evnt/InsertViewModelEvnt.kt
@@ -1,6 +1,27 @@
 package com.example.finalpam.ui.viewmodel.Evnt
 
 
+import com.example.finalpam.entitas.Event
+
+
+
+
+fun InsertUiEvntEvent.toEvnt(): Event = Event (
+    id_event = id_event ?: 0,
+    nama_event = nama_event,
+    deskripsi_event = deskripsi_event,
+    tanggal_event = tanggal_event,
+    lokasi_event = lokasi_event
+)
+
+
+fun Event.toInsertUiEvntEvent(): InsertUiEvntEvent = InsertUiEvntEvent (
+    id_event = id_event,
+    nama_event = nama_event,
+    deskripsi_event = deskripsi_event,
+    tanggal_event = tanggal_event,
+    lokasi_event = lokasi_event
+)
 
 data class InsertUiEvntEvent(
     val id_event: Int ?= 0,
