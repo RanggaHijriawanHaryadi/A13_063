db1a6b2ae2b8f2f05d7beb8272ff70b7cbe6726b
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-27 19:15:36 +0700

    Menambahkan class HomeUiPesertaState dengan success,error,loading

--- app/src/main/java/com/example/finalpam/ui/viewmodel/Pstr/HomeViewModelPstr.kt
+++ app/src/main/java/com/example/finalpam/ui/viewmodel/Pstr/HomeViewModelPstr.kt
@@ -1,10 +1,18 @@
 package com.example.finalpam.ui.viewmodel.Pstr
 
+
 import androidx.lifecycle.ViewModel
+import com.example.finalpam.entitas.Peserta
 import com.example.finalpam.repository.PesertaRepository
 
-
+sealed class HomeUiPesertaState {
+    data class Success(val peserta: List<Peserta>) : HomeUiPesertaState()
+    object Error : HomeUiPesertaState()
+    object Loading : HomeUiPesertaState()
+}
 
 class HomeViewModelPstr(
     private val pst: PesertaRepository
-) : ViewModel()
+) : ViewModel(){
+
+}
