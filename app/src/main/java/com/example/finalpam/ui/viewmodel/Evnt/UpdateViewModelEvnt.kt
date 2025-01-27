82abea523ee144ca62c5c68063fd9a132d4e5dec
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-28 04:48:41 +0700

    Membuat var updateUiEvntState dan val id_event

--- app/src/main/java/com/example/finalpam/ui/viewmodel/Evnt/UpdateViewModelEvnt.kt
+++ app/src/main/java/com/example/finalpam/ui/viewmodel/Evnt/UpdateViewModelEvnt.kt
@@ -1,15 +1,29 @@
 package com.example.finalpam.ui.viewmodel.Evnt
 
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-
+import com.example.finalpam.repository.EventRepository
+import com.example.finalpam.ui.navigation.DestinasiUpdateEvnt
 import kotlinx.coroutines.launch
 
 class UpdateViewModelEvnt(
 
 ): ViewModel() {
+        var updateUiEvntState by mutableStateOf(InsertUiEvntState())
+        private set
 
+    private val id_event: Int = checkNotNull(savedStateHandle[DestinasiUpdateEvnt.Id_Event])
+
+    init {
+        viewModelScope.launch {
+            updateUiEvntState = evnt.getEventById(id_event).data
+                .toUiStateEvnt()
+        }
+    }
 
     fun updateEventState(insertUiEvntEvent: InsertUiEvntEvent){
         updateUiEvntState = InsertUiEvntState(insertUiEvntEvent =  insertUiEvntEvent)
