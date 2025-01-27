d34bb45a6b761bd6ceabe49dfac7a4d28d115a18
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-27 20:46:25 +0700

    Menambahkan InsertUiPesertaEvent dengan data peserta

--- app/src/main/java/com/example/finalpam/ui/viewmodel/Pstr/InsertViewModelPstr.kt
+++ app/src/main/java/com/example/finalpam/ui/viewmodel/Pstr/InsertViewModelPstr.kt
@@ -1,6 +1,25 @@
 package com.example.finalpam.ui.viewmodel.Pstr
 
 
+import com.example.finalpam.entitas.Peserta
+
+
+
+
+fun InsertUiPesertaEvent.toPstr(): Peserta = Peserta (
+    id_peserta = id_peserta ?: 0,
+    nama_peserta = nama_peserta,
+    email = email,
+    nomor_telepon = nomor_telepon,
+)
+
+
+fun Peserta.toInsertUiPstrEvent(): InsertUiPesertaEvent = InsertUiPesertaEvent (
+    id_peserta = id_peserta,
+    nama_peserta = nama_peserta,
+    email = email,
+    nomor_telepon = nomor_telepon,
+)
 
 data class InsertUiPesertaEvent(
     val id_peserta: Int ?= 0,
