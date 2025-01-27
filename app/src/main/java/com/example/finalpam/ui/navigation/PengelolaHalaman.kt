dad62b3823712be37435af077edece62f1705d4f
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-27 22:22:09 +0700

    Membuat Detail peserta dengan navigasi DestinasiDetailPstr

--- app/src/main/java/com/example/finalpam/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/finalpam/ui/navigation/PengelolaHalaman.kt
@@ -3,12 +3,28 @@ package com.example.finalpam.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.finalpam.ui.view.Event.DetailViewEvent
+import com.example.finalpam.ui.view.Event.HomeViewEvent
+import com.example.finalpam.ui.view.Event.InsertViewEvent
+import com.example.finalpam.ui.view.Event.UpdateViewEvent
+import com.example.finalpam.ui.view.HalamanUtamaView.HalamanUtamaView
+import com.example.finalpam.ui.view.Tikets.DetailViewTikets
+import com.example.finalpam.ui.view.peserta.DetailViewPstr
 import com.example.finalpam.ui.view.peserta.HomeViewPeserta
 import com.example.finalpam.ui.view.peserta.InsertViewPstr
-
+import com.example.finalpam.ui.view.peserta.UpdateViewPstr
+import com.example.finalpam.ui.view.Tikets.HomeViewTikets
+import com.example.finalpam.ui.view.Tikets.InsertViewTikets
+import com.example.finalpam.ui.view.Tikets.UpdateViewTikets
+import com.example.finalpam.ui.view.Transaksi.DetailViewTransaksi
+import com.example.finalpam.ui.view.Transaksi.HomeViewTransaksi
+import com.example.finalpam.ui.view.Transaksi.InsertViewTransaksi
+import com.example.finalpam.ui.view.Transaksi.UpdateViewTransaksi
 
 
 @Composable
@@ -41,6 +57,27 @@ fun PengelolaHalaman(
                 navigateBack = {
                 navController.popBackStack()
             })
+        }
+
+        composable(
+            DestinasiDetailPstr.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiDetailPstr.Id_Peserta){
+                    type = NavType.IntType
+                }
+            )
+        ) {
+            val id_peserta = it.arguments?.getInt(DestinasiDetailPstr.Id_Peserta)
+            id_peserta?.let {
+                DetailViewPstr(
+                    onBackClick = {
+                        navController.popBackStack()
+                    },
+                    onEditPstrClick =  {
+                        navController.navigate("${DestinasiUpdatePstr.route}/$id_peserta")
+                                       },
+                )
+            }
         }
     }
 }
