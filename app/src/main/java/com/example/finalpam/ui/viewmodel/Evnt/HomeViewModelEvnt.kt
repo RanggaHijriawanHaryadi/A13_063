a2aefdf69aa5af38418649a3f0576c93be4811ee
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-28 04:23:13 +0700

    Menambahkan class HomeUiEventState dengan success,error,loading

--- app/src/main/java/com/example/finalpam/ui/viewmodel/Evnt/HomeViewModelEvnt.kt
+++ app/src/main/java/com/example/finalpam/ui/viewmodel/Evnt/HomeViewModelEvnt.kt
@@ -3,10 +3,15 @@ package com.example.finalpam.ui.viewmodel.Evnt
 
 import androidx.lifecycle.ViewModel
 
+import com.example.finalpam.entitas.Event
 import com.example.finalpam.repository.EventRepository
 
 
-
+sealed class HomeUiEventState {
+    data class Success(val event: List<Event>) : HomeUiEventState()
+    object Error : HomeUiEventState()
+    object Loading : HomeUiEventState()
+}
 
 class HomeViewModelEvnt(
     private val evt: EventRepository
