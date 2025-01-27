d40fd518b5903479c02c4c4d638bdc5f1580f3fb
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-27 19:16:41 +0700

    Menambahkan fun deletePstr dengan id_peserta

--- app/src/main/java/com/example/finalpam/ui/viewmodel/Pstr/HomeViewModelPstr.kt
+++ app/src/main/java/com/example/finalpam/ui/viewmodel/Pstr/HomeViewModelPstr.kt
@@ -2,8 +2,12 @@ package com.example.finalpam.ui.viewmodel.Pstr
 
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.example.finalpam.entitas.Peserta
 import com.example.finalpam.repository.PesertaRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed class HomeUiPesertaState {
     data class Success(val peserta: List<Peserta>) : HomeUiPesertaState()
@@ -15,4 +19,16 @@ class HomeViewModelPstr(
     private val pst: PesertaRepository
 ) : ViewModel(){
 
+
+    fun deletePstr(id_peserta: Int) {
+        viewModelScope.launch {
+            try {
+                pst.deletePeserta(id_peserta)
+            } catch (e: IOException) {
+                HomeUiPesertaState.Error
+            } catch (e: HttpException) {
+                HomeUiPesertaState.Error
+            }
+        }
+    }
 }
