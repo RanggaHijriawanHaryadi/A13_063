59187d00b00104daea819c8762749634ae037f78
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-28 05:34:34 +0700

    Membuat fungsi InsertViewEvent dengan topAppbar dan innserpadding

--- app/src/main/java/com/example/finalpam/ui/view/Event/InsertViewEvent.kt
+++ app/src/main/java/com/example/finalpam/ui/view/Event/InsertViewEvent.kt
@@ -4,20 +4,69 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalpam.ui.costumewidget.TopAppBar
+import com.example.finalpam.ui.navigation.DestinasiInsertEvnt
 import com.example.finalpam.ui.viewmodel.Evnt.InsertUiEvntEvent
 
 import com.example.finalpam.ui.viewmodel.Evnt.InsertUiEvntState
+import com.example.finalpam.ui.viewmodel.Evnt.InsertViewModelEvnt
+import com.example.finalpam.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InsertViewEvent(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModelEvnt = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = DestinasiInsertEvnt.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){ innerPadding ->
+        InsertBodyEvnt(
+            insertUiEventState =  viewModel.uiStateEvnt,
+            onEvntValueChange = viewModel::updateInsertEvntState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertEvnt()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
 @Composable
 fun InsertBodyEvnt(
