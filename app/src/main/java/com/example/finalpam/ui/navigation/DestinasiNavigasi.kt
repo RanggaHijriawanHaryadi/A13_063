a1df7fa737b8b579c11c54506beb9143e5b04ff5
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-27 22:19:41 +0700

    Membuat Navigasi pada Detail Peserta dengan Id_Peserta

--- app/src/main/java/com/example/finalpam/ui/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/finalpam/ui/navigation/DestinasiNavigasi.kt
@@ -20,4 +20,10 @@ object DestinasiInsertPstr: DestinasiNavigasi{
     override val route= "Insert Peserta"
     override val titleRes = "Tambah data Peserta"
 }
+object DestinasiDetailPstr : DestinasiNavigasi {
+    override val route= "detail peserta"
+    override val titleRes = "Detail Peserta"
+    const val Id_Peserta = "id"
+    val routeWithArgs = "$route/{$Id_Peserta}"
+}
 
