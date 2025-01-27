dd1b21ef8dc7233c94d54db8da4066d0026896a3
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-27 18:41:51 +0700

    Menambahkan CRUD dengan val pesertaApiService pada NetworkPesertaRepository

--- app/src/main/java/com/example/finalpam/repository/PesertaRepository.kt
+++ app/src/main/java/com/example/finalpam/repository/PesertaRepository.kt
@@ -3,6 +3,8 @@ package com.example.finalpam.repository
 import com.example.finalpam.entitas.Peserta
 import com.example.finalpam.entitas.PesertaResponse
 import com.example.finalpam.entitas.PesertaResponseDetail
+import com.example.finalpam.service.PesertaService
+import java.io.IOException
 
 interface PesertaRepository {
     suspend fun getPeserta(): PesertaResponse
@@ -12,3 +14,38 @@ interface PesertaRepository {
     suspend fun getPesertaById(id_peserta: Int):PesertaResponseDetail
 }
 
+class NetworkPesertaRepository(
+    private val pesertaApiService: PesertaService
+) : PesertaRepository{
+
+    override suspend fun getPeserta(): PesertaResponse {
+        return pesertaApiService.getPeserta()
+    }
+
+    override suspend fun insertPeserta(peserta: Peserta) {
+        pesertaApiService.insertPeserta(peserta)
+    }
+
+    override suspend fun updatePeserta(id_peserta: Int, peserta: Peserta) {
+        pesertaApiService.updatePeserta(id_peserta, peserta)
+    }
+
+    override suspend fun deletePeserta(id_peserta: Int) {
+        try {
+            val response = pesertaApiService.deletePeserta(id_peserta)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete peserta. HTTP Status Code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getPesertaById(id_peserta: Int): PesertaResponseDetail {
+        return  pesertaApiService.getPesertaById(id_peserta)
+    }
+}
