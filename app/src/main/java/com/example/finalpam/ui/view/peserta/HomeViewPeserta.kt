da17c7a74097b59a2702ffe1733f835606c4a18e
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-27 19:52:28 +0700

    Membuat Homestatus peserta, loding, error

--- app/src/main/java/com/example/finalpam/ui/view/peserta/HomeViewPeserta.kt
+++ app/src/main/java/com/example/finalpam/ui/view/peserta/HomeViewPeserta.kt
@@ -1,7 +1,9 @@
 package com.example.finalpam.ui.view.peserta
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -17,6 +19,7 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Person
 
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -31,11 +34,77 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.finalpam.R
 import com.example.finalpam.entitas.Peserta
+import com.example.finalpam.ui.viewmodel.Pstr.HomeUiPesertaState
 
 
 
+@Composable
+fun HomePstrStatus(
+    homeUiPesertaState: HomeUiPesertaState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Peserta) -> Unit = {},
+    onDetailPstrClick: (Int) -> Unit
+){
+    when (homeUiPesertaState) {
+        is HomeUiPesertaState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+        is HomeUiPesertaState.Success ->
+            if (homeUiPesertaState.peserta.isEmpty()){
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Peserta" )
+                }
+            } else {
+                PesertaLayout(
+                    peserta = homeUiPesertaState.peserta, modifier = modifier.fillMaxWidth(),
+                    onDetailPstrClick = {
+                        onDetailPstrClick(it.id_peserta)
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    }
+                )
+            }
+        is HomeUiPesertaState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun OnLoading(
+    modifier: Modifier = Modifier
+) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.l),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.e), contentDescription = ""
+        )
+        Text(
+            text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp)
+        )
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
 
 @Composable
 fun PesertaLayout(
