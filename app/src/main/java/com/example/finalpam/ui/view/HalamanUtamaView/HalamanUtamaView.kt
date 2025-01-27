c420ee154c58d4e11402694f5bc9513baa09119a
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-28 04:50:45 +0700

    Membuat HalamanUtamaView dengan lazy column dan button halaman Event

--- app/src/main/java/com/example/finalpam/ui/view/HalamanUtamaView/HalamanUtamaView.kt
+++ app/src/main/java/com/example/finalpam/ui/view/HalamanUtamaView/HalamanUtamaView.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,17 +14,22 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 
-
-
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -104,7 +110,19 @@ fun HalamanUtamaView(
                     fontStyle = FontStyle.Italic
                   )
                 }
-
+                // Home Event
+                Button(
+                  onClick = onEventClick,
+                  modifier = Modifier.fillMaxWidth()
+                    .padding(8.dp),
+                  colors = ButtonDefaults.buttonColors(Color(0xFF87CEEB))
+                ) {
+                  Text(
+                    text = "Home Event",
+                    fontSize = 20.sp,
+                    fontStyle = FontStyle.Italic
+                  )
+                }
               }
             }
           }
