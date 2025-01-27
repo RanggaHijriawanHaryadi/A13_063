a9549c6d76065849cc4e535b6d92dfc29d9f0c0e
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-27 22:06:01 +0700

    Membuat fungsi InsertViewPstr dengan topAppbar dan innserpadding

--- app/src/main/java/com/example/finalpam/ui/view/peserta/InsertViewPstr.kt
+++ app/src/main/java/com/example/finalpam/ui/view/peserta/InsertViewPstr.kt
@@ -4,21 +4,70 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalpam.ui.costumewidget.TopAppBar
+import com.example.finalpam.ui.navigation.DestinasiInsertPstr
 import com.example.finalpam.ui.viewmodel.Pstr.InsertUiPesertaEvent
 import com.example.finalpam.ui.viewmodel.Pstr.InsertUiPesertaState
+import com.example.finalpam.ui.viewmodel.Pstr.InsertViewModelPstr
+import com.example.finalpam.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InsertViewPstr(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModelPstr = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = DestinasiInsertPstr.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){ innerPadding ->
+        InsertBodyPstr(
+            insertUiPesertaState =  viewModel.uiStatePstr,
+            onPstrValueChange = viewModel::updateInsertPstrState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertPstr()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
 @Composable
 fun InsertBodyPstr(
