8f9e60a70904228cdf73aa129b6e16c82512a340
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-27 20:52:59 +0700

    Membuat Navigasi pada Insert Peserta

--- app/src/main/java/com/example/finalpam/ui/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/finalpam/ui/navigation/DestinasiNavigasi.kt
@@ -5,8 +5,19 @@ interface DestinasiNavigasi {
     val titleRes: String
 }
 
+// Halaman Utama
+object DestinasiHalamanUtama: DestinasiNavigasi{
+    override val route = "Utama"
+    override val titleRes = "Halaman Utama"
+}
+
 // Halaman Home Peserta
 object DestinasiHomePstr : DestinasiNavigasi {
     override val route = "Peserta"
     override val titleRes = "Halaman Peserta"
 }
+object DestinasiInsertPstr: DestinasiNavigasi{
+    override val route= "Insert Peserta"
+    override val titleRes = "Tambah data Peserta"
+}
+
