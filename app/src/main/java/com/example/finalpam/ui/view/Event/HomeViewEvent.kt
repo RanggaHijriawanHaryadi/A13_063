37859dd8bbb1829277a681de0e064d5cb4ed5ccc
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-28 05:29:57 +0700

    Membuat Homestatus event, loding, error

--- app/src/main/java/com/example/finalpam/ui/view/Event/HomeViewEvent.kt
+++ app/src/main/java/com/example/finalpam/ui/view/Event/HomeViewEvent.kt
@@ -1,8 +1,9 @@
 package com.example.finalpam.ui.view.Event
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -17,6 +18,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -32,13 +34,82 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.finalpam.R
 import com.example.finalpam.entitas.Event
+import com.example.finalpam.ui.viewmodel.Evnt.HomeUiEventState
 
 
 
 
+@Composable
+fun HomeEvntStatus(
+    homeUiEventState: HomeUiEventState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Event) -> Unit = {},
+    onDetailClick: (Int) -> Unit
+){
+    when (homeUiEventState) {
+        is HomeUiEventState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+        is HomeUiEventState.Success ->
+            if (homeUiEventState.event.isEmpty()){
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Event" )
+                }
+            } else {
+                EventLayout(
+                    event = homeUiEventState.event, modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.id_event)
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    }
+                )
+            }
+        is HomeUiEventState.Error -> OnError(
+            retryAction,
+            modifier = modifier.fillMaxSize()
+        )
+    }
+}
+
+@Composable
+fun OnLoading(
+    modifier: Modifier = Modifier
+) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.l),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.e), contentDescription = ""
+        )
+        Text(
+            text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp)
+        )
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
 
 @Composable
 fun EventLayout(
