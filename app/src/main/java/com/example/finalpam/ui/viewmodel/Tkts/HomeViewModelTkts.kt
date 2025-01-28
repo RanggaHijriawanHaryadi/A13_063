2c52c99e316faa3f565059bbb4abd6b2f9d3c5b1
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-28 07:09:35 +0700

    Membuat fungsi deleteTkts dengan id_tikets dan fun getTkts

--- app/src/main/java/com/example/finalpam/ui/viewmodel/Tkts/HomeViewModelTkts.kt
+++ app/src/main/java/com/example/finalpam/ui/viewmodel/Tkts/HomeViewModelTkts.kt
@@ -1,13 +1,19 @@
 package com.example.finalpam.ui.viewmodel.Tkts
 
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.finalpam.entitas.Event
+import com.example.finalpam.entitas.Peserta
+import com.example.finalpam.entitas.Tikets
 import com.example.finalpam.repository.EventRepository
 import com.example.finalpam.repository.PesertaRepository
 import com.example.finalpam.repository.TiketsRepository
-
-
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 
 
@@ -17,4 +23,33 @@ class HomeViewModelTkts (
     private val event: EventRepository
 ):ViewModel(){
 
+
+    init {
+        getTkts()
+    }
+
+    fun getTkts(){
+        viewModelScope.launch {
+            tktsUIState = HomeUiTikesState.Loading
+            tktsUIState = try {
+                HomeUiTikesState.Success(tkt.getTikets().data)
+            } catch (e: IOException) {
+                HomeUiTikesState.Error
+            } catch (e: HttpException){
+                HomeUiTikesState.Error
+            }
+        }
+    }
+
+    fun deleteTkts(id_tikets: Int) {
+        viewModelScope.launch {
+            try {
+                tkt.deleteTikets(id_tikets)
+            } catch (e: IOException) {
+                HomeUiTikesState.Error
+            } catch (e: HttpException) {
+                HomeUiTikesState.Error
+            }
+        }
+    }
 }
