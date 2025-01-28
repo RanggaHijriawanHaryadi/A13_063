95ec8649d8b3fd7e4220ad748c58ab6afa4d1531
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-28 20:27:36 +0700

    Menambahkan warna blue dan mengatur TopAppBar

--- app/src/main/java/com/example/finalpam/ui/costumewidget/TopAppBar.kt
+++ app/src/main/java/com/example/finalpam/ui/costumewidget/TopAppBar.kt
@@ -1,6 +1,9 @@
 package com.example.finalpam.ui.costumewidget
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Refresh
@@ -9,9 +12,13 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.finalpam.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -31,13 +38,13 @@ fun TopAppBar(
             })
         },
 
-        modifier = modifier,
+        modifier = modifier.fillMaxWidth().padding(top = 20.dp),
         scrollBehavior = scrollBehavior, navigationIcon = {
             if(canNavigateBack){
                 IconButton(onClick = navigateUp) {
                     Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
                 }
             }
-        }
+        }, colors = TopAppBarDefaults.topAppBarColors(containerColor = colorResource(id = R.color.blue))
     )
 }
