141b79842ec0e2f51e3302aa53188e4d960054d0
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-28 15:23:15 +0700

    Menambahkan CRUD dengan val transaksiApiService pada NetworkTransaksiRepository

---
+++ app/src/main/java/com/example/finalpam/repository/TransaksiRepository.kt
@@ -0,0 +1,43 @@
+package com.example.finalpam.repository
+
+import com.example.finalpam.entitas.Transaksi
+import com.example.finalpam.entitas.TransaksiResponse
+import com.example.finalpam.entitas.TransaksiResponseDetail
+import com.example.finalpam.service.TransaksiService
+import java.io.IOException
+
+class NetworkTransaksiRepository (
+    private val transaksiApiService: TransaksiService
+): TransaksiRepository{
+    override suspend fun getTransaksi(): TransaksiResponse {
+        return transaksiApiService.getTransaksi()
+    }
+
+    override suspend fun getTransaksiById(id_transaksi: Int): TransaksiResponseDetail {
+        return transaksiApiService.getTransaksiById(id_transaksi)
+    }
+
+    override suspend fun insertTransaksi(transaksi: Transaksi) {
+        transaksiApiService.insertTransaksi(transaksi)
+    }
+
+    override suspend fun updateTransaksi(id_transaksi: Int, transaksi: Transaksi) {
+        transaksiApiService.updateTransaksi(id_transaksi, transaksi)
+    }
+
+    override suspend fun deleteTransaksi(id_transaksi: Int) {
+        try {
+            val response = transaksiApiService.deleteTransaksi(id_transaksi)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete Tiket. HTTP Status Code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception){
+            throw e
+        }
+    }
+
+}
